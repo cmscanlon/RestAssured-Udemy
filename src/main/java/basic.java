94f32f4d8bd93c2c8fc3d082786814330374cb0b
Carolyn <cmscanlon@yahoo.com>
2020-09-15 11:18:20 -0500

    Fixed dependency issues -CMS

--- src/main/java/basic.java
+++ src/main/java/basic.java
@@ -1,7 +1,5 @@
 import io.restassured.RestAssured;
-
 import static io.restassured.RestAssured.*;
-import static org.hamcrest.Matchers.*;
 
 public class basic {
 
@@ -11,13 +9,22 @@ public class basic {
         //given - all input details
         //when - Submit the API -resource,http method
         //then - validate the response
-        RestAssured.baseURI= "https://rahulshettyacademy.com";
-        given().log().all().queryParam("key", "qaclick123").header("Content-Type","application/json")
-                .body("{\r\n" +
-                        "\"place_id\":\""+placeId+"\",\r\n" +
-                        "\"address\":\""+newAddress+"\",\r\n" +
-                        "\"key\":\"qaclick123\"\r\n" +
+        RestAssured.baseURI = "https://rahulshettyacademy.com";
+        given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json")
+                .body("{\n" +
+                        "    \"location\": {\n" +
+                        "        \"latitude\": \"-38.383494\",\n" +
+                        "        \"longitude\": \"33.427362\"\n" +
+                        "    },\n" +
+                        "    \"accuracy\": \"50\",\n" +
+                        "    \"name\": \"Frontline house\",\n" +
+                        "    \"phone_number\": \"(+91) 983 893 3937\",\n" +
+                        "    \"address\": \"70 Summer Walk, USA\",\n" +
+                        "    \"types\": \"shoe park,shop\",\n" +
+                        "    \"website\": \"http://google.com\",\n" +
+                        "    \"language\": \"French-IN\"\n" +
                         "}")
-                .when().put("maps/api/place/update/json")
+                .when().post("maps/api/place/update/json")
                 .then().assertThat().log().all().statusCode(200);
+    }
 }
